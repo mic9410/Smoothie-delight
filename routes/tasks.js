4cf1bb780a0e96d875961c0977d43632e3e5000e
michal <mic132@gazeta.pl>
2017-05-13 20:53:45 +0200

    Working database

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,9 +3,16 @@
  */
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://admin:admin@ds025459.mlab.com:25459/smoothie',['mailing_list']);
 
-router.get('/tasks', function (req, res, next) {
-    res.send('TASKS API');
+router.get('/mailing_list', function (req, res, next) {
+    db.mailing_list.find(function (err, mailing_list) {
+        if(err){
+            res.send(err);
+        }
+        res.json(mailing_list);
+    });
 })
 
 module.exports = router;
