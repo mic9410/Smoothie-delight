cb29cd7ec69301dd10cf012766a9ec5edc01518b
michal <mic132@gazeta.pl>
2017-05-13 21:13:43 +0200

    Added putting email address on the server side .

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,6 +6,7 @@ var router = express.Router();
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://admin:admin@ds025459.mlab.com:25459/smoothie',['mailing_list']);
 
+// Get All addresses
 router.get('/mailing_list', function (req, res, next) {
     db.mailing_list.find(function (err, mailing_list) {
         if(err){
@@ -15,4 +16,37 @@ router.get('/mailing_list', function (req, res, next) {
     });
 })
 
+//Get single address
+router.get('/mailing_list/:id', function (req, res, next) {
+    db.mailing_list.findOne({_id: mongojs.ObjectId(req.params.id)}, function (err, email_address) {
+        if(err){
+            res.send(err);
+        }
+        res.json(email_address);
+    });
+})
+
+// Email validation
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return re.test(email);
+}
+
+//Save email address
+router.post('/mailing_list', function (req, res, next) {
+    var address = req.body;
+    if(!address.name || !validateEmail(address.email)){
+        res.status(400);
+        res.json({
+            "error": "Username or mail is not correct"
+        })
+    }else{
+        db.mailing_list.save(address,function (err, address) {
+            if(err){
+                res.send(err);
+            }
+            res.json(address);
+        });
+    }
+})
 module.exports = router;
